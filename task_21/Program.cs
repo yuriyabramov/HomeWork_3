5df426634fafd172240fc5be6bcb0906f427f49e
Yury_Lomatov <yury.lomatov@yandex.ru>
2023-05-04 19:35:35 +0300

    task_21

--- task_21/Program.cs
+++ task_21/Program.cs
@@ -15,7 +15,7 @@ int z2 = int.Parse(Console.ReadLine() ?? "");
 
 double distance = GetDistance(x1, y1, z1, x2, y2, z2);
 
-Console.WriteLine($"{distance:f2}");
+Console.WriteLine($"A ({x1}, {y1}, {z1}); B ({x2}, {y2}, {z2}) ->  {distance:f2}");
 
 
 static double GetDistance(int x1, int y1, int z1, int x2, int y2, int z2)
@@ -29,20 +29,3 @@ static double GetDistance(int x1, int y1, int z1, int x2, int y2, int z2)
 
 
 
-/*double result = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
-Console.WriteLine($"A ({x1}, {y1}, {z1}); B ({x2}, {y2}, {z2}) -> result:f2");
-
-static int GetUserNumder(string message);
-{
-    while (true)
-    {
-        Console.Write();
-        int userInput;
-        if (int.TryParse(Console.ReadLine(), out userInput))
-        {
-            return userInput;
-        }
-        else Console.WriteLine("Incorrect data");
-    }
-    
-}*/

6220951397c9bd5f42b448b40bb23a37e8f4b6af
Yury_Lomatov <yury.lomatov@yandex.ru>
2023-05-04 19:33:22 +0300

    task_19

--- task_19/Program.cs
+++ task_19/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом. Например: 14212 -> нет; 12821 -> да.
+
+int num = new Random(). Next (10000, 99999);
+string number = num.ToString();
+
+
+
+if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.WriteLine($"{number} - Палиндром");
+    }
+else 
+    {
+        
+        Console.WriteLine($"{number} - НЕ палиндром");
+    }
+
+
+
+
+
